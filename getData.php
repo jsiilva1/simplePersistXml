b74bd88f2996e284efc659b08a70dac7b700eca9
jsiilva1 <jsiilva@outlook.com.br>
2019-05-12 14:56:02 -0300

    Implementada a view dos dados

---
+++ getData.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Lista dos clientes</title>
+    <link rel="stylesheet" href="./static/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./static/css/all.min.css">
+  </head>
+  <body>
+
+    <div class="container-fluid">
+      <h1>Lista dos Clientes</h1>
+
+      <table class="table">
+        <thead>
+              <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">E-mail</th>
+                <th scope="col">Fone</th>
+                <th scope="col">Endereço</th>
+                <th scope="col">Profissão</th>
+                <th scope="col">Formação</th>
+                <th scope="col">Estado Civil</th>
+                <th scope="col">Tipo Sanguíneo</th>
+                <th scope="col">Ação</th>
+              </tr>
+        </thead>
+        <tbody>
+          <?php
+            $xmldoc = simplexml_load_file("./database.xml") or die("Falha ao carregar o arquivo de dados");
+            foreach($xmldoc->children() as $client) {
+          ?>
+          <tr>
+            <td><?php echo $client->name;?></td>
+            <td><?php echo $client->email;?></td>
+            <td><?php echo $client->phone;?></td>
+            <td><?php echo $client->address;?></td>
+            <td><?php echo $client->profession;?></td>
+            <td><?php echo $client->formation;?></td>
+            <td><?php echo $client->maritalStatus;?></td>
+            <td><?php echo $client->bloodType;?></td>
+            <td>
+              <a href="" title="Editar"><i class="far fa-edit"></i></a> |
+              <a href="" title="Excluir"><i class="fas fa-trash-alt"></i></a>
+            </td>
+          </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </div>
+
+    <script src="./static/js/bootstrap.min.js"></script>
+    <script src="./static/js/fontawesome.min.js"></scri
+  </body>
+</html>
--- p-xml.php
+++ p-xml.php
@@ -1,19 +1,19 @@
 <?php
-
 $xmldoc = new DomDocument('1.0');
 $xmldoc->preserveWhiteSpace = true;
 $xmldoc->formatOutput = true;
 
 $id = rand();
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$telefone = $_POST['telefone'];
-$endereco = $_POST['endereco'];
-$profissao = $_POST['profissao'];
-$formacao = $_POST['formacao'];
-$estado_civil = $_POST['estado_civil'];
-$tipo_sanguineo = $_POST['tipo_sanguineo'];
+$nome = $_POST['nome'] ?? null;
+$email = $_POST['email']?? null;
+$telefone = $_POST['telefone']?? null;
+$endereco = $_POST['endereco']?? null;
+$profissao = $_POST['profissao']?? null;
+$formacao = $_POST['formacao'] ?? null;
+$estado_civil = $_POST['estado_civil'] ?? null;
+$tipo_sanguineo = $_POST['tipo_sanguineo'] ?? null;
 
+// Traz o arquivo para a memória
 $file = file_get_contents('./database.xml');
 
 if($file) {
@@ -22,29 +22,64 @@ if($file) {
   $root = $xmldoc->getElementsByTagName('maincontent')->item(0);
 
   $cliente = $xmldoc->createElement("cliente");
-  $numAttribute = $xmldoc->createAttribute("id");
-  $numAttribute->value = $id;
-  $cliente->appendChild($numAttribute);
 
   $root->insertBefore($cliente, $root->firstChild);
 
-  // Name node
+  // Nó do nome
+  $idElement = $xmldoc->createElement('id');
+  $cliente->appendChild($idElement);
+  $idText = $xmldoc->createTextNode($id);
+  $idElement->appendChild($idText);
+
+  // Nó do nome
   $nameElement = $xmldoc->createElement('name');
   $cliente->appendChild($nameElement);
   $nameText = $xmldoc->createTextNode($nome);
   $nameElement->appendChild($nameText);
 
-  // Email node
+  // Nó do email
   $emailElement = $xmldoc->createElement('email');
   $cliente->appendChild($emailElement);
   $emailText = $xmldoc->createTextNode($email);
   $emailElement->appendChild($emailText);
 
   // Phone node
-  $emailElement = $xmldoc->createElement('phone');
-  $cliente->appendChild($emailElement);
-  $emailText = $xmldoc->createTextNode($email);
-  $emailElement->appendChild($emailText);
+  $phoneElement = $xmldoc->createElement('phone');
+  $cliente->appendChild($phoneElement);
+  $phoneText = $xmldoc->createTextNode($telefone);
+  $phoneElement->appendChild($phoneText);
+
+  // Nó do endereço
+  $addressElement = $xmldoc->createElement('address');
+  $cliente->appendChild($addressElement);
+  $addressText = $xmldoc->createTextNode($endereco);
+  $addressElement->appendChild($addressText);
+
+  // Nó da profissão
+  $professionElement = $xmldoc->createElement('profession');
+  $cliente->appendChild($professionElement);
+  $professionText = $xmldoc->createTextNode($profissao);
+  $professionElement->appendChild($professionText);
+
+  // Nó da formação
+  $formationElement = $xmldoc->createElement('formation');
+  $cliente->appendChild($formationElement);
+  $formationText = $xmldoc->createTextNode($formacao);
+  $formationElement->appendChild($formationText);
+
+  // Nó do estado civil
+  $maritalStatusElement = $xmldoc->createElement('maritalStatus');
+  $cliente->appendChild($maritalStatusElement);
+  $maritalStatusText = $xmldoc->createTextNode($estado_civil);
+  $maritalStatusElement->appendChild($maritalStatusText);
+
+  // Nó do tipo sanguineo
+  $bloodTypeElement = $xmldoc->createElement('bloodType');
+  $cliente->appendChild($bloodTypeElement);
+  $bloodTypeText = $xmldoc->createTextNode($tipo_sanguineo);
+  $bloodTypeElement->appendChild($bloodTypeText);
 
   $xmldoc->save('./database.xml');
+
+  header('location: getData.php');
 }
